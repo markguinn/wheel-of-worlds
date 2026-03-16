e2d1809af967c64ba4b0092f6fb64c0ecdbffc00
davids91 <toth.david.munka@gmail.com>
2026-03-16 21:44:27 +0100

    feat: shadow-blob second iteration

--- common/shadow_blob.gd
+++ common/shadow_blob.gd
@@ -1,17 +1,14 @@
+class_name ShadowBlob
 extends Node2D
 
 @export var point_probes: Array[ShadowProbe]
 @export var test_probes: Array[ShadowProbe]
 @export var probe_radius_px: float = 100.
-@export var loops_to_wait_for_probes: int = 200
+@export var loops_to_wait_for_probes: int = 5
 @export var line: Line2D
 
 ## Measurements for light at position: {"pos": Vector2, "light": float}
 var light_measurements: Array[Dictionary] = []
-
-#TODO: move center within the points
-#TODO: queue_free when center is in light for some time
-#TODO: Experiment with moving the monster with the help of the shadowcompass
 
 func _scatter_probe(array: Array[ShadowProbe], i: int, angle_increment: float, radius: float = probe_radius_px) -> void:
 	var current_angle = i * angle_increment + randf() * angle_increment
@@ -36,11 +33,66 @@ func _ready() -> void:
 		light_measurements.push_back({"pos": point_probes[i].global_position, "light": point_probes[i].lit_value})
 		_scatter_probe(test_probes, i, angle_increment)
 		light_measurements.push_back({"pos": test_probes[i].global_position, "light": test_probes[i].lit_value})
-	_init_debug_image()
+	if debug_image: _init_debug_image()
 
+@export var can_be_in_light_for_sec: float = 1.
+@export var shadow_value_threshold: float = -0.4
+var loop_count_in_probe_state: int = 0
+var in_light_since: float = 0.
+func _process(delta: float) -> void:
+	if has_node("DebugLabel"):$DebugLabel.set_text(str($CenterProbe.lit_value))
+	if loop_count_in_probe_state < loops_to_wait_for_probes:
+		loop_count_in_probe_state += 1
+		return
 
+	# Only execute every `loops_to_wait_for_probes` loops
+	loop_count_in_probe_state = 0
+	var angle_increment = 2. * PI / float(point_probes.size())
+	var center_position: Vector2 = Vector2.ZERO
+	for i in test_probes.size():
+		# Check and compare probes to currently established boundary points!
+		var distance_to_probe: float = (test_probes[i].global_position - global_position).length()
+		if( # Probe is darker, than the current point
+			test_probes[i].lit_value < point_probes[i].lit_value
+			or ( # probe is at the same light level, but further away
+				test_probes[i].lit_value == point_probes[i].lit_value
+				and(distance_to_probe >= (point_probes[i].global_position - global_position).length())
+			) # Test probe is in a better position than the probe
+		): point_probes[i].global_position = test_probes[i].global_position
+		light_measurements[i] = {"pos": point_probes[i].global_position, "light": point_probes[i].lit_value}
+		light_measurements[point_probes.size() + i] = {"pos": test_probes[i].global_position, "light": test_probes[i].lit_value}
+
+		# Scatter the test probes again
+		_scatter_probe(test_probes, i, angle_increment, 250.)
+
+		# add some offset to the lines
+		$DebugLine2.points[i] = test_probes[i].global_position + Vector2(512, 0)
+		
+		# Move Points closer to darkness based on mesurements if the point is in light
+		if shadow_value_threshold < point_probes[i].lit_value:
+			var to_shadow: Vector2 = ShadowCompass.get_direction_to_darkness(
+				light_measurements, $CenterProbe.lit_value, point_probes[i].global_position
+			) * 10.
+			if (point_probes[i].global_position + to_shadow - global_position).length() <= probe_radius_px:
+				point_probes[i].global_position += to_shadow
+
+		line.points[i] = point_probes[i].global_position
+		center_position += point_probes[i].global_position
+
+	if shadow_value_threshold < $CenterProbe.lit_value:
+		in_light_since += delta
+		if in_light_since >= can_be_in_light_for_sec: queue_free()
+	else: in_light_since = 0.
+
+	center_position /= float(point_probes.size())
+	global_position = center_position
+	if debug_image: _update_debug_image()  # repaint after every compare cycle
+	
+	
 
 #region debug image
+@export var debug_image: bool = false
+
 const DEBUG_IMAGE_SIZE = 512
 const DEBUG_IMAGE_HALF = DEBUG_IMAGE_SIZE / 2.
 const DEBUG_IMAGE_WORLD_SIZE = 250.
@@ -87,60 +139,3 @@ func _paint_sample(world_pos: Vector2, light_value: float) -> void:
 		return
 	_debug_image.set_pixel(DEBUG_IMAGE_SIZE - round(px), DEBUG_IMAGE_SIZE - round(py), heat_color(light_value))
 #endregion
-
-var loop_count_in_probe_state: int = 0
-func _process(_delta: float) -> void:
-	if loop_count_in_probe_state < loops_to_wait_for_probes:
-		loop_count_in_probe_state += 1
-		return
-	
-	# Only execute every `loops_to_wait_for_probes` loops
-	loop_count_in_probe_state = 0
-	var angle_increment = 2. * PI / float(point_probes.size())
-	var center_position: Vector2 = Vector2.ZERO
-	for i in test_probes.size():
-		#print(
-			#"%s: %s(d: %s) <> %s: %s(d: %s)"
-			#% [
-				#test_probes[i].global_position, test_probes[i].lit_value, (test_probes[i].global_position - global_position).length(),
-				#point_probes[i].global_position, point_probes[i].lit_value, (point_probes[i].global_position - global_position).length()
-			#]
-		#)
-		# Check and compare probes to currently established boundary points!
-		var distance_to_probe: float = (test_probes[i].global_position - global_position).length()
-		if( # Probe is darker, than the current point
-			test_probes[i].lit_value < point_probes[i].lit_value
-			or ( # probe is at the same light level, but further away
-				test_probes[i].lit_value == point_probes[i].lit_value
-				and(distance_to_probe >= (point_probes[i].global_position - global_position).length())
-			)
-			## probe is at least ligther, but closer to the origin
-			## this means that there is light inbetween two points, so formation needs to be corrected
-			#or (
-				#test_probes[i].lit_value > point_probes[i].lit_value * 2.
-				#and distance_to_probe < (point_probes[i].global_position - global_position).length()
-			#)
-		): # Test probe is in a better position than the probe
-			#print("====== TEST PROBE IS BETTER!")
-			point_probes[i].global_position = test_probes[i].global_position
-		light_measurements[i] = {"pos": point_probes[i].global_position, "light": point_probes[i].lit_value}
-		light_measurements[point_probes.size() + i] = {"pos": test_probes[i].global_position, "light": test_probes[i].lit_value}
-
-		# Scatter the test probes again
-		_scatter_probe(test_probes, i, angle_increment, 250.)
-		#test_probes[i].global_position += Vector2(randf() - 0.5, randf() - 0.5) * 10.
-		# add some offset to the lines
-		$DebugLine2.points[i] = test_probes[i].global_position + Vector2(512, 0)
-		
-		# Move Points closer to darkness based on mesurements
-		var to_shadow: Vector2 = ShadowCompass.get_direction_to_darkness(
-			light_measurements, $CenterProbe.lit_value, global_position
-		) * 10.
-		if (point_probes[i].global_position + to_shadow - global_position).length() <= probe_radius_px:
-			point_probes[i].global_position += to_shadow
-
-		line.points[i] = point_probes[i].global_position #+ Vector2(512, 0)
-		center_position += point_probes[i].global_position
-	center_position /= float(point_probes.size())
-	global_position = center_position
-	_update_debug_image()  # repaint after every compare cycle
--- common/shadow_probe.gd
+++ common/shadow_probe.gd
@@ -8,69 +8,22 @@ extends Sprite2D
 @export var lit_value: float = 0.0:
 	get(): return 0. if is_nan(lit_value) else lit_value
 
-func _process(delta: float) -> void:
-	#lit_value = -get_viewport().get_texture().get_image().get_pixelv(
-		#get_global_transform_with_canvas().get_origin()  - Vector2(45., 109.)
-	#).get_luminance()
-	if has_node("DebugLabel"): $DebugLabel.text = "%.3f" % lit_value
-	#if has_node("DebugLabel"): $DebugLabel.text = str(
-		#get_global_transform_with_canvas().get_origin()
-	#)
+#TODO: inconsistent values when viewport is not on default size
 
-func _ready() -> void:
-	# we need to save the viewport position of the sprite before drawing,
-	# otherwise the position will be off and we might not mask to the sprite correctly.
-	RenderingServer.frame_pre_draw.connect(_on_viewport_frame_pre_draw)
-	# and we need to do the light check after drawing.
-	RenderingServer.frame_post_draw.connect(_on_viewport_frame_post_draw)
-
-var last_sprite_tex: Texture = null # texture before frame draw.
-var last_crop_rect: Rect2i = Rect2i() # position before frame draw.
-var pixel_to_compare: Color = Color.WHITE
-func _on_viewport_frame_pre_draw() -> void:
-	#pixel_to_compare = get_viewport().get_texture().get_image().get_pixelv(
-		#get_global_transform_with_canvas().get_origin()
-	#)
-	#pixel_to_compare /= max(0.001, pixel_to_compare.a)
-	last_sprite_tex = texture
-	var viewport_scale: Vector2 = get_viewport_transform().get_scale()
-	var screen_pos = get_screen_transform().origin * viewport_scale
-	last_crop_rect = Rect2i(
-		screen_pos-((last_sprite_tex.get_size()/2.0)*viewport_scale),
-		last_sprite_tex.get_size()*viewport_scale
-	)
-
+## The original color of the 1x1 texture
+var luminance_base: float = Color.from_rgba8(128,128,128,255).get_luminance()
+func _process(_delta: float) -> void: if has_node("DebugLabel"): $DebugLabel.text = "%.3f" % lit_value
+func _ready() -> void: RenderingServer.frame_post_draw.connect(_on_viewport_frame_post_draw)
 func _on_viewport_frame_post_draw() -> void:
-	#var pixel_after_render: Color = get_viewport().get_texture().get_image().get_pixelv(
-		#get_global_transform_with_canvas().get_origin()
-	#)
-	#pixel_after_render /= max(0.001, pixel_after_render.a)
-	##lit_value = round((pixel_after_render.get_luminance() - pixel_to_compare.get_luminance()) / 0.05) * 0.05
-	#lit_value = ((pixel_after_render.get_luminance() - pixel_to_compare.get_luminance()))
-	if last_sprite_tex == null:
-		return Color(0,0,0,0)
-	var view_img: Image = get_viewport().get_texture().get_image()
-	var sprite_img: Image = last_sprite_tex.get_image()
-	view_img = view_img.get_region(last_crop_rect)
-	# resize the image back down to sprite level scale;
-	# I think you might only need to do this if your project stretch_mode is canvas_items. But I'm unsure.
-	view_img.resize(last_sprite_tex.get_size().x, last_sprite_tex.get_size().y)
-	# Convert the image to match the last_sprite_tex format, otherwise we won't be allowed to mask with it.
-	view_img.convert(sprite_img.get_format())
-	var final_img = Image.create_empty(last_sprite_tex.get_size().x, last_sprite_tex.get_size().y, false, Image.FORMAT_RGBA8)
-	# Use the sprite_img to mask now.
-	final_img.blit_rect_mask(view_img, sprite_img, Rect2i(Vector2i(), last_sprite_tex.get_size()), Vector2i())
-	# Important! if the alpha isn't fixed, the final color will be off, and the whole thing will break.
-	final_img.fix_alpha_edges()
-	# Resize them down so you can get their total color, I found INTERPOLATE_LANCZOS to be the only method to work.
-	final_img.resize(1, 1, Image.INTERPOLATE_LANCZOS)
-	sprite_img.resize(1, 1, Image.INTERPOLATE_LANCZOS)
-	# Get the view color
-	var final_color = final_img.get_pixel(0, 0)
-	# Get the sprites color as well, this way we can compare luminance levels.
-	var base_color = sprite_img.get_pixel(0, 0)
-	# We need to divide the colors by their alpha, otherwise the color will be muted from empty space.
-	final_color = (final_color/max(final_color.a, 0.0001))
-	base_color = (base_color/max(base_color.a, 0.0001))
-	# We then need to subtract the base_color to compare the two colors luminance. Which will be the lit_value.
-	lit_value = round((final_color.get_luminance() - base_color.get_luminance()) / 0.05) * 0.05
+	var canvas_origin: Vector2 = get_global_transform_with_canvas().get_origin()
+	var viewport_image: Image = get_viewport().get_texture().get_image()
+	if ( # Do not probe shadow when position is out of viewport
+		0. > canvas_origin.x || 0. > canvas_origin.y
+		|| viewport_image.get_size().x < canvas_origin.x
+		|| viewport_image.get_size().y < canvas_origin.y
+	):
+		lit_value = 0.
+		return
+	var pixel_after_render: Color = viewport_image.get_pixelv(canvas_origin)
+	pixel_after_render /= max(0.001, pixel_after_render.a) # We need to divide the colors by their alpha, otherwise the color will be muted from empty space
+	lit_value = (pixel_after_render.get_luminance() - luminance_base)
