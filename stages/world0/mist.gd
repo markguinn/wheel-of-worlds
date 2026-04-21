6943d43852997dfda1d8d0715f5f1fe7572f2bf3
Mark Guinn <markguinn@gmail.com>
2026-04-20 23:48:41 -0700

    Added some more mist to world0 for atmosphere

---
+++ stages/world0/mist.gd
@@ -0,0 +1,27 @@
+extends Sprite2D
+
+@export var min_lacunity := 3.0
+@export var max_lacunity := 6.0
+@export var tick_delta := 0.001
+@export var ms_per_frame := 60
+
+var last_tick := 0
+var noise : FastNoiseLite
+
+func _ready() -> void:
+	var tex: NoiseTexture2D = self.texture
+	noise = tex.noise
+
+
+func _process(_delta: float) -> void:
+	if GameManager.now_ms() > last_tick + ms_per_frame:
+		# NOTE: this is beatiful but also slows the web build down to a crawl.
+		# TODO: can we just export a few frames of this as static files?
+		#noise.fractal_lacunarity += tick_delta
+		#if noise.fractal_lacunarity > max_lacunity:
+			#noise.fractal_lacunarity = max_lacunity
+			#tick_delta = -abs(tick_delta)
+		#if noise.fractal_lacunarity < min_lacunity:
+			#noise.fractal_lacunarity = min_lacunity
+			#tick_delta = abs(tick_delta)
+		last_tick = GameManager.now_ms()
