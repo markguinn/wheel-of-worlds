155a9e14b55af5c33563d70aca7bc93406024786
Keith Smyth <smyth.keith@gmail.com>
2026-05-16 08:20:14 -0400

    giant prototype

---
+++ stages/test/giant_test.gd
@@ -0,0 +1,21 @@
+extends Stage
+
+var time: float = 0
+
+func _on_ready() -> void:
+	$ParallaxBush/AnimatedSprite2D.play("default")
+
+func _physics_process(delta: float) -> void:
+	time += delta
+	if time > 3:
+		time = 0
+		_jump_props(delta)
+		$ParallaxBush/AnimatedSprite2D.play("default")
+
+
+func _jump_props(delta: float) -> void:
+	print("jump-props")
+	$Plank.apply_impulse(Vector2(0, -40), $Plank.global_position)
+	$Plank2.apply_impulse(Vector2(0, -40), $Plank2.global_position)
+	$Orb.apply_impulse(Vector2(0, -0.5), $Orb.global_position)
+	$Player.velocity.y += -50
