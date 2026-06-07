e08c44a9d78bd04021d72b93b1f9fa196db6e972
Mark Guinn <markguinn@gmail.com>
2026-06-06 23:38:51 -0700

    Pull in the real water textures from the world0 design

---
+++ common/water_children.gd
@@ -0,0 +1,34 @@
+@tool
+extends Control
+
+
+@export var speed := 1.0
+@export var amount := 10.0
+@export var wave_layers: Array[TextureRect] = []
+@export var wave_offset := Vector2(0.0, -120.0)
+
+var rnd_layers: Array[float] = []
+var start_layers: Array[Vector2] = []
+var t := 0.0
+
+
+func _ready() -> void:
+	if wave_layers.size() == 0:
+		for n in get_children():
+			if n is TextureRect:
+				wave_layers.append(n)
+	for l in wave_layers:
+		rnd_layers.append(randf())
+
+
+func _process(delta: float) -> void:
+	t += delta * speed
+	for i in range(wave_layers.size()):
+		var my_t := t + rnd_layers[i]
+		var my_dir: float = 1.0 if i % 2 == 0 else -1.0
+		var my_amount: float = amount * lerp(0.8, 1.2, rnd_layers[i])
+		var my_offset := Vector2(
+			cos(my_t * my_dir) * my_amount + lerp(-200.0, 200.0, rnd_layers[i]),
+			sin(my_t * my_dir) * my_amount
+		)
+		wave_layers[i].position = wave_offset + my_offset
