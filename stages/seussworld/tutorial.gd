132f867ec0865328b60104458b5bcd84cab14d3b
Mark Guinn <markguinn@gmail.com>
2026-07-12 13:09:09 -0700

    tutorial level - text, backgrounds, music zones

---
+++ stages/seussworld/tutorial.gd
@@ -0,0 +1,15 @@
+extends Stage
+
+func _ready() -> void:
+	get_tree().create_timer(1.0).timeout.connect(_stand_up)
+
+#func _input(event: InputEvent) -> void:
+	#if event.is_pressed():
+		#var p := GameManager.get_player()
+		#p.perma_ragdoll = false
+		#await get_tree().physics_frame
+		#p._input(event)
+
+func _stand_up() -> void:
+	var p := GameManager.get_player()
+	p.perma_ragdoll = false
