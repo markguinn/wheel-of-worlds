0f3442bc42757dd3911c4ea6879b373b8b79fff3
Mark Guinn <markguinn@gmail.com>
2026-07-11 18:54:00 -0700

    fix mist on forest level

--- stages/seussworld/mist.gd
+++ stages/seussworld/mist.gd
@@ -13,15 +13,15 @@ func _ready() -> void:
 	noise = tex.noise
 
 
-func _process(_delta: float) -> void:
-	if GameManager.now_ms() > last_tick + ms_per_frame:
-		# NOTE: this is beatiful but also slows the web build down to a crawl.
-		# TODO: can we just export a few frames of this as static files?
-		#noise.fractal_lacunarity += tick_delta
-		#if noise.fractal_lacunarity > max_lacunity:
-			#noise.fractal_lacunarity = max_lacunity
-			#tick_delta = -abs(tick_delta)
-		#if noise.fractal_lacunarity < min_lacunity:
-			#noise.fractal_lacunarity = min_lacunity
-			#tick_delta = abs(tick_delta)
-		last_tick = GameManager.now_ms()
+#func _process(_delta: float) -> void:
+	#if GameManager.now_ms() > last_tick + ms_per_frame:
+		## NOTE: this is beatiful but also slows the web build down to a crawl.
+		## TODO: can we just export a few frames of this as static files?
+		##noise.fractal_lacunarity += tick_delta
+		##if noise.fractal_lacunarity > max_lacunity:
+			##noise.fractal_lacunarity = max_lacunity
+			##tick_delta = -abs(tick_delta)
+		##if noise.fractal_lacunarity < min_lacunity:
+			##noise.fractal_lacunarity = min_lacunity
+			##tick_delta = abs(tick_delta)
+		#last_tick = GameManager.now_ms()
